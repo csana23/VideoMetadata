e8e7aa3b3a97abdaea64e08a5446a14db95efb62
Richard Csanaki <richard.csanaki@gmail.com>
2020-07-21 21:13:10 +0200

    Update video.py

--- src/video.py
+++ src/video.py
@@ -1,5 +1,25 @@
 import ffmpeg
+from pprint import pprint
 
-video = ffmpeg.probe('C:\\Users\\richa\\Videos\\video.mp4')
+def converter(input_file_path, output_file_path):
+    try:
+        stream = ffmpeg.input(input_file_path)
+        stream = ffmpeg.output(stream, output_file_path)
+        ffmpeg.run(stream)
 
-print(video['streams'])
+        print('Converting video successful')
+    except FileNotFoundError as error:
+        print(error)
+
+def get_metadata(file_path):
+    try:
+        video = ffmpeg.probe(str(file_path))
+        meta = video['streams']
+
+        pprint(meta)
+    except FileNotFoundError as error:
+        print(error)
+
+converter('C:/Users/richa/Videos/small.webm', 'C:/Users/richa/Videos/small.mp4')
+get_metadata('C:/Users/richa/Videos/small.mp4')
+
