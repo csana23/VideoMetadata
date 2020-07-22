d1710b3e4d8ebde4cb9f8747e3a38924e9ee658f
Richard Csanaki <richard.csanaki@gmail.com>
2020-07-22 20:21:28 +0200

    Update video.py

--- src/video.py
+++ src/video.py
@@ -1,5 +1,8 @@
 import ffmpeg
 from pprint import pprint
+import cv2
+import numpy as np
+import os
 
 def converter(input_file_path, output_file_path):
     try:
@@ -20,6 +23,38 @@ def get_metadata(file_path):
     except FileNotFoundError as error:
         print(error)
 
-converter('C:/Users/richa/Videos/small.webm', 'C:/Users/richa/Videos/small.mp4')
-get_metadata('C:/Users/richa/Videos/small.mp4')
+# sample images from video
+def sample_images(video_file_path, data_folder_path):
+    video = cv2.VideoCapture(video_file_path)
+
+    try:
+        if not os.path.exists(data_folder_path):
+            os.makedirs(data_folder_path)
+
+    except OSError:
+        print ('Error: Creating directory of data')
+
+    current_frame = 0
+    factor = video.get(cv2.CAP_PROP_FPS)
+
+    while True:
+        ret, frame = video.read()
+
+        file_name = str(data_folder_path) + '/frame' + str(current_frame) + '.jpg'
+        print('Creating...' + file_name)
+        cv2.imwrite(file_name, frame)
+
+        current_frame += 1 * factor
+
+    video.release()
+    cv2.destroyAllWindows()
+
+# get sample images
+
+
+
+# converter('C:/Users/richa/Videos/small.webm', 'C:/Users/richa/Videos/small.mp4')
+# get_metadata('C:/Users/richa/Videos/small.mp4')
+
+
 
